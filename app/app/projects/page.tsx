40afb1fab9d3216014419faeede46c3793b680ae
Angus-Moore-Dev <angusmoore.dev@gmail.com>
2024-11-20 10:30:27 +1000

    supabase logo for external link on projects card

--- app/app/projects/page.tsx
+++ app/app/projects/page.tsx
@@ -3,7 +3,8 @@ import CreateNewProject from '@/components/projects/CreateNewProject';
 import { Button, Card } from '@mantine/core';
 import Link from 'next/link';
 import { ExternalLink } from 'lucide-react';
-
+import SupabaseLogo from '@/public/supabase_logo_transparent.png';
+import Image from 'next/image';
 
 export default async function ProjectsPage()
 {
@@ -41,9 +42,16 @@ export default async function ProjectsPage()
         <div className='grid grid-cols-3 gap-5'>
             {
                 data.map(project => <Card key={project.id} padding={'lg'} radius={'lg'} withBorder bg={'#2a2a2a'} className='flex flex-col gap-3    '>
-                    <h3>
-                        {project.databaseName}
-                    </h3>
+                    <section className='flex gap-3 items-start justify-between'>
+                        <h3>
+                            {project.databaseName}
+                        </h3>
+                        <Link target='_blank' href={`https://www.supabase.com/dashboard/project/${project.projectId}`}>
+                            <Button variant='subtle' rightSection={<ExternalLink size={16} />}>
+                                <Image src={SupabaseLogo} alt='Supabase Logo' width={16} height={16} />
+                            </Button>
+                        </Link>
+                    </section>
                     <small className='text-neutral-500 font-medium'>
                         <b>{project.projectId}</b> | {project.notebooks.length} Notebooks | {project.saved_user_macros.length} Macros
                     </small>
@@ -51,11 +59,6 @@ export default async function ProjectsPage()
                         <Link href={`/app/${project.id}`}>
                             <Button>
                                 Enter Notebook
-                            </Button>
-                        </Link>
-                        <Link target='_blank' href={`https://www.supabase.com/dashboard/project/${project.projectId}`}>
-                            <Button variant='subtle' rightSection={<ExternalLink size={16} />}>
-                                Open in Supabase
                             </Button>
                         </Link>
                     </section>
