41e482084b3dd06bc9913d9ae35e58cba475b5ef
Angus-Moore-Dev <angusmoore.dev@gmail.com>
2024-11-22 14:02:51 +1000

    using system font

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,12 +5,11 @@ import '@mantine/charts/styles.css';
 import './globals.css';
 import './loading_animation.css';
 import { ColorSchemeScript, MantineProvider } from '@mantine/core';
-import { Raleway } from 'next/font/google';
 import { Notifications } from '@mantine/notifications';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
-const inter = Raleway({ subsets: ['latin'] });
+// const inter = Raleway({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
     title: 'Supacord',
@@ -43,7 +42,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
                 <ColorSchemeScript color="dark" defaultColorScheme='dark' forceColorScheme="dark" />
                 <script async defer src="https://buttons.github.io/buttons.js"></script>
             </head>
-            <body className={`${inter.className} min-h-screen flex flex-col antialiased`}>
+            <body className={'min-h-screen flex flex-col antialiased'}>
                 <MantineProvider defaultColorScheme="dark" theme={{
                     primaryColor: 'green',
                     fontSmoothing: true,
